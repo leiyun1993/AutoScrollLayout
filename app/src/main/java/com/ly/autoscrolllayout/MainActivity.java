fe96270be420c22cf2020d3740faf3a0e973da5e
Administrator <waitshan@163.com>
2017-07-28 17:39:04 +0800

    用简单的方式增加几种切换效果

--- app/src/main/java/com/ly/autoscrolllayout/MainActivity.java
+++ app/src/main/java/com/ly/autoscrolllayout/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     private VerticalScrollTextView vTextView;
     private VerticalScrollLayout vScrollLayout;
 
